0a42cf68ae13228adce3e36c15f440179cb9c09c
Andrew7567 <andrei.petrukhin@yandex.ru>
2022-04-26 08:33:09 +0300

    new

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -1,8 +1,8 @@
+from yatube.settings import POSTS_ON_PAGE
 from django import forms
 from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
-from yatube.settings import POSTS_ON_PAGE
 from django.core.cache import cache
 
 from posts.forms import PostForm
